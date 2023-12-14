3d52f46c656768c6c525be5836161102652bcfce
Gian <gianmnf.dev@gmail.com>
2023-12-14 12:50:54 -0300

    First Tetromino

--- src/main/PlayManager.java
+++ src/main/PlayManager.java
@@ -6,6 +6,8 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
+import mino.*;
+
 public class PlayManager {
 
 	// Main Play Area
@@ -16,6 +18,11 @@ public class PlayManager {
 	public static int top_y;
 	public static int bottom_y;
 	
+	// Mino
+	Mino currentMino;
+	final int MINO_START_X;
+	final int MINO_START_Y;	
+	
 	public PlayManager() {
 		
 		// Main Play Area Frame
@@ -24,10 +31,15 @@ public class PlayManager {
 		top_y = 50;
 		bottom_y = top_y + HEIGHT;
 		
+		MINO_START_X = left_x + (WIDTH/2) - Block.SIZE;
+		MINO_START_Y = top_y + Block.SIZE;
+		
+		currentMino = new Mino_L1();
+		currentMino.setXY(MINO_START_X, MINO_START_Y);
 	}
 	
 	public void update() {
-		
+		currentMino.update();
 	}
 	
 	public void draw(Graphics2D g2) {
@@ -45,5 +57,8 @@ public class PlayManager {
 		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		g2.drawString("NEXT", x+60, y+60);
 		
+		if (currentMino != null) {		
+			currentMino.draw(g2);
+		}
 	}
 }
--- src/mino/Mino_L1.java
+++ src/mino/Mino_L1.java
@@ -18,7 +18,7 @@ public class Mino_L1 extends Mino{
 		b[1].y = b[0].y - Block.SIZE;
 		b[2].x = b[0].x;
 		b[2].y = b[0].y + Block.SIZE;
-		b[3].y = b[0].x + Block.SIZE;
+		b[3].x = b[0].x + Block.SIZE;
 		b[3].y = b[0].y + Block.SIZE;
 	}
 }
