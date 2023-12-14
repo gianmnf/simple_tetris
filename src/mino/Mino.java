fae2de97041dcc9223ddabc0a9f541d94c0f3e66
Gian <gianmnf.dev@gmail.com>
2023-12-14 16:14:54 -0300

    Rotation

--- src/mino/Mino.java
+++ src/mino/Mino.java
@@ -9,9 +9,9 @@ import main.PlayManager;
 public class Mino {
 	
 	public Block b [] = new Block[4];
-	public Block tempB[] = new Block[4];
-	
+	public Block tempB[] = new Block[4];	
 	int autoDropCounter = 0;
+	public int direction = 1;
 	
 	public void create (Color c) {
 		b[0] = new Block(c);
@@ -24,14 +24,35 @@ public class Mino {
 		tempB[3] = new Block(c);
 	}
 	
-	public void setXY(int x, int y) {}
-	
-	public void updateXY(int direction) {}
+	public void setXY(int x, int y) {}	
+	public void updateXY(int direction) {
+		
+		this.direction = direction;
+		b[0].x = tempB[0].x;
+		b[0].y = tempB[0].y;
+		b[1].x = tempB[1].x;
+		b[1].y = tempB[1].y;
+		b[2].x = tempB[2].x;
+		b[2].y = tempB[2].y;
+		b[3].x = tempB[3].x;
+		b[3].y = tempB[3].y;
+		
+	}	
+	public void getDirection1() {}	
+	public void getDirection2() {}	
+	public void getDirection3() {}	
+	public void getDirection4() {}
 	
 	public void update() {
 		
 		if (KeyHandler.upPressed) {
-			
+			switch(direction) {
+			 case 1: getDirection2();break;
+			 case 2: getDirection3();break;
+			 case 3: getDirection4();break;
+			 case 4: getDirection1();break;
+			}
+			KeyHandler.upPressed = false;
 		}
 		
 		if (KeyHandler.downPressed) {
